1e95a393b308996d3a5c7e3c0a164f778121946d
edciriac <eduard.d.ciriaco@student.jyu.fi>
2022-07-04 16:22:27 -0400

    Añadir el scraper de opinion de chiloe

--- source/insert_medios.py
+++ source/insert_medios.py
@@ -3,3 +3,4 @@ from insert_manual_mariadb import insert_medio
 insert_medio("Diario Los Lagos","https://www.diarioloslagos.cl" , "Puerto Montt", "español")
 insert_medio( "El Repuertero", "https://www.elrepuertero.cl","Puerto Montt", "español")
 insert_medio("Diario De Puerto Montt", "https://www.diariodepuertomontt.cl", "Puerto Montt", "español")
+insert_medio("La Opinión de Chiloé", "https://laopiniondechiloe.cl", "Chiloé", "español")
