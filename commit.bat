5f04d368e939374b3a7342d8ebe726284f396103
edciriac <eduard.d.ciriaco@student.jyu.fi>
2022-07-05 10:52:30 -0400

    Agregar un script para hacer commits

---
+++ commit.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+git status
+
+:choice
+set /P c= Quieres agregar todo al commmit? [Y/N]?
+if /I "%c%" EQU "Y" goto :add_all_commit_push
+if /I "%c%" EQU "N" goto :stop_script
+goto :choice
+
+:add_all_commit_push
+echo "Comiteando todo"
+git add --all
+set /p msg="Commit message:"
+echo %msg%
+git commit -m "%msg%"
+pause
+exit
+
+:stop_script
+echo "Saliendo sin commits"
+pause
+exit
