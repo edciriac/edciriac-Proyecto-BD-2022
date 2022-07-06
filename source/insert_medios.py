b12b797ecdd573e898025d2b2eb41d0e05867f14
Ekku Sipilä <ekku.eki@gmail.com>
2022-07-06 10:23:17 -0400

    Eliminar el inutil script de insert_manual_medios

--- source/insert_medios.py
+++
@@ -1,7 +0,0 @@
-from insert_manual_mariadb import insert_medio
-
-insert_medio("Diario Los Lagos","https://www.diarioloslagos.cl" , "Puerto Montt", "español")
-insert_medio( "El Repuertero", "https://www.elrepuertero.cl","Puerto Montt", "español")
-insert_medio("Diario De Puerto Montt", "https://www.diariodepuertomontt.cl", "Puerto Montt", "español")
-insert_medio("La Opinión de Chiloé", "https://laopiniondechiloe.cl", "Chiloé", "español")
-insert_medio("Noticias Chiloé", "https://www.noticiaschiloe.cl", "Chiloé", "español")
